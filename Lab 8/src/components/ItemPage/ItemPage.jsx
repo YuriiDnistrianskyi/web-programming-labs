d82fc482c47c6d1d6547e98bdf3821e1febf9d0b
YuraDnistriansky <yura.dniester@gmail.com>
2024-11-11 23:38:15 +0200

    Update lab 8
    Update itemPage

--- Lab 8/src/components/ItemPage/ItemPage.jsx
+++ Lab 8/src/components/ItemPage/ItemPage.jsx
@@ -15,10 +15,18 @@ function ItemPage(item) {
                             <div>Option 2</div>
                         </div>
                         <h1 className="content-name">{item.name} Name</h1>
-                        <div>
-
+                        <div className="content-info">
+                            <div className="info">Audience: {item.audience} 20000</div>
+                            <div className="info">Lighting Power: {item.lightingPower} 2000 lux</div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="item__bottom">
+                <div className="item__bottom-price">Price: {item.price} 999$</div>
+                <div className="item__bottom-buttons">
+                    <button className="button-item-page go-back">Go Back</button>
+                    <button className="button-item-page add-to">Add to Card</button>
                 </div>
             </div>
         </section>
